c3dfac615555fa8a427d72a5481351e279366cb1
codevarius <koshaevk@gmail.com>
2019-11-07 01:39:26 +0300

    bug fix

--- app/src/main/java/com/kshv/example/jargogle_app/ui/main/MainFragment.java
+++ app/src/main/java/com/kshv/example/jargogle_app/ui/main/MainFragment.java
@@ -202,6 +202,7 @@ public class MainFragment extends Fragment {
             }
             adapter.updateJargogleList (provider.getJargogleList ());
             adapter.notifyItemRemoved (position);
+            onResume();
         }
     }
 }
